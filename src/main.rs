e7aad53b8975ef8974f137b9d91d0b0a4bff26d3
Roy Larson <roy.larson@gmail.com>
2021-10-29 22:12:47 -0500

    convert rest of user input to using dialuguer

--- src/main.rs
+++ src/main.rs
@@ -1,41 +1,29 @@
 mod instructions;
-// mod players;
-// mod game_board;
 
 use instructions::query_to_display_instructions;
 use std::io;
 use startraders::Player;
-
+use dialoguer::Input;
 
 fn main() {
     println!("              **********   STAR TRADERS   **********");
     query_to_display_instructions();
-    let mut input = String::new();
+
     let mut players:Vec<Player> = Vec::new();
     let mut player:Player;
-    let mut num_players:u32 = 0;
+    let num_players:u32 = Input::new()
+        .with_prompt("How many players are playing:")
+        .default(1)
+        .interact()
+        .unwrap();
 
-    println!("How many people are playing?");
-    loop {
-        io::stdin().read_line(&mut input).unwrap();
-        input = input.chars().filter(|c| !c.is_whitespace()).collect();
-        num_players = match input.parse::<u32>(){
-            Ok(n) => n,
-            Err(_) => 0
-        };
-
-        if (0<num_players) && (num_players<=4) {
-            break
-        } 
-        println!("Num players need to be between 1 and 4: input={}", &num_players);
-    }
 
     for i in 0..num_players{
-        println!("Please input players {} name:", i+1);
-        let mut input = String::new();
-        io::stdin().read_line(&mut input).unwrap();
-        input = input.trim().to_string();
-        let player = Player::new(input);
+        let name = Input::<String>::new()
+            .with_prompt(format!("What is player {}'s name",i+1))
+            .interact()
+            .unwrap();
+        let player = Player::new(name);
         players.push(player);
     }
     println!("Players: {:?}", players)
