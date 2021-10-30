ff2cefbfe1a628446d5e6956acc1ef2470c77231
Roy Larson <roy.larson@gmail.com>
2021-10-29 20:50:21 -0500

    create Player

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,46 @@
+use std::collections::HashMap;
 
+#[derive(Debug, PartialEq, Eq, Hash)]
+pub enum Company{
+    ALTAIR,
+    BETELGEUSE,
+    CAPELLA,
+    DENEBOLA,
+    ERIDANI,
+}
 
-pub mod instructions;
+#[derive(Debug, PartialEq, Eq)]
+pub struct Player {
+    name: String,
+    balance: usize,
+    stocks: HashMap<Company,usize>,    
+}
+
+impl Player{
+    pub fn new(name:String)->Player{
+        let mut stocks = HashMap::new();
+        stocks.insert(Company::ALTAIR,0);
+        stocks.insert(Company::BETELGEUSE,0);
+        stocks.insert(Company::CAPELLA,0);
+        stocks.insert(Company::DENEBOLA,0);
+        stocks.insert(Company::ERIDANI,0);
+        Player{name:name, balance:0, stocks:stocks}
+
+    }
+}
+
+#[derive(Debug, PartialEq, Eq, Hash)]
+struct Location{
+    x: usize,
+    y: usize
+}
+
+// pub struct Board {
+//     company_loc: HashMap<
+// }
+
+// impl Board {
+//     pub fn new()->Board {
+
+//     }
+// }
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,16 @@
 mod instructions;
+// mod players;
+// mod game_board;
 
 use instructions::query_to_display_instructions;
 use std::io;
+use startraders::Player;
 
 
 fn main() {
+    println!("              **********   STAR TRADERS   **********");
     query_to_display_instructions();
+    let player = Player::new("Roy".to_string());
+    println!("Player: {:?}", player);
 
 }
