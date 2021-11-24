e06124435e16480114eec71c602f652c5d1757c2
Roy Larson <roy.larson@gmail.com>
2021-11-23 19:52:03 -0600

    implement Location::from_str

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,7 @@
 use std::collections::HashMap;
-use std::borrow::Cow;
 use rand::prelude::*;
 use rand_pcg::Pcg64;
+use std::str::FromStr;
 
 
 
@@ -29,7 +29,7 @@ impl Player{
         stocks.insert(Company::CAPELLA,0);
         stocks.insert(Company::DENEBOLA,0);
         stocks.insert(Company::ERIDANI,0);
-        Player{name:name, balance:100, stocks:stocks}
+        Player{name, balance:100, stocks}
 
     }
 }
@@ -40,11 +40,70 @@ pub enum LocationOccupancy{
     COMPANY(Company)
 }
 
-#[derive(Clone, Copy, Debug, PartialEq, Eq, Hash)]
+#[derive(Clone, Debug, PartialEq, Eq, Hash)]
 pub struct Location{
-    pub x: char,
-    pub y: char
+    pub x: String,
+    pub y: String,
 }
+
+#[derive(Debug, Clone)]
+pub enum ParsePointError {
+    FailedParse(String),
+    Not2Dimensional(usize),
+    NonNumeric,
+}
+
+impl FromStr for Location {
+    type Err = ParsePointError;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let clean_s = s
+            .trim_matches(|p| p == '(' || p == ')')
+            .trim()
+            .replace(|p| p == ' ', "");
+        {
+            if !clean_s.contains(
+                |c| {
+                        ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0'].contains(&c)
+                    }
+                ) 
+                {
+                return Err(ParsePointError::NonNumeric);
+                }
+        }
+        let coords:String = clean_s.chars().collect::<String>();
+
+        if coords.len() != 2 {
+            return Err(ParsePointError::Not2Dimensional(coords.len()));
+        }
+        let x = Some(match &coords.chars().nth(0) {
+            Some(c) => c.to_string(),
+            None => return Err(ParsePointError::FailedParse(format!("X value is not a char "))),
+            }
+        );
+
+        let y = Some(match &coords.chars().nth(1) {
+            Some(c) => c.to_string(),
+            None => return Err(ParsePointError::FailedParse(format!("Y value is not a char"))),
+            }
+         );
+        
+        if y.is_none() || x.is_none() {
+            return Err(ParsePointError::FailedParse(format!("Not enough coordinates to be valid: {:?}", &coords)));
+        }
+        Ok(Location {x:x.unwrap(), y:y.unwrap()})
+        
+    }
+}
+
+#[test]
+fn test_location_from_str(){
+    let loc = Location::from_str("A1").unwrap();
+    assert_eq!(&loc.x, "A");
+    assert_eq!(&loc.y, "1");
+
+}
+
 
 pub struct Board {
     pub spaces: HashMap<Location,LocationOccupancy>
@@ -61,9 +120,9 @@ impl Board {
             for n in rows.chars(){
                 let rand_num:f64 = rng.gen();
                 if rand_num<0.05{
-                    spaces.insert(Location{x:l, y:n}, LocationOccupancy::STAR);
+                    spaces.insert(Location{x:l.to_string(), y:l.to_string()}, LocationOccupancy::STAR);
                 } else {
-                    spaces.insert(Location{x:l, y:n}, LocationOccupancy::OPEN);
+                    spaces.insert(Location{x:l.to_string(), y:n.to_string()}, LocationOccupancy::OPEN);
                 }
             }
         }
@@ -71,7 +130,7 @@ impl Board {
         Board{spaces:spaces}
 
     }
-    pub fn get_legal_moves(&self){
+    pub fn get_legal_moves(&self)->Vec<Location> {
         let mut open_locations= Vec::<Location>::new();
         for (location, occupation) in &self.spaces{
             match occupation{
@@ -81,7 +140,11 @@ impl Board {
                 _=>{}
             }
         }
-        println!("Options\n{:?}", open_locations.choose_multiple(&mut rand::thread_rng(), 5).collect::<Vec<&Location>>().to_owned());
+        let mut moves = Vec::<Location>::new();
+        for loc in open_locations.choose_multiple(&mut rand::thread_rng(), 6){
+            moves.push(loc.clone())
+        }
+        moves
     }
 }
 
