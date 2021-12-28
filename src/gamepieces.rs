57db1f35eb3d5e3323f33e2e9263f3dc0253d56d
Roy Larson <roy.larson@gmail.com>
2021-12-27 21:39:17 -0600

    changes moves to be named struct

--- src/gamepieces.rs
+++ src/gamepieces.rs
@@ -21,24 +21,40 @@ impl fmt::Display for LocationOccupancy {
 }
 
 #[derive(Debug, Clone)]
-pub struct Moves(pub Vec<Location>);
+pub struct Moves {
+    moves: Vec<Location>,
+}
+
+impl Default for Moves {
+    fn default() -> Self {
+        Moves {
+            moves: Vec::<Location>::new(),
+        }
+    }
+}
 
 impl fmt::Display for Moves {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         let mut comma_separated = String::new();
 
-        for loc in &self.0[0..self.0.len() - 1] {
+        for loc in &self.moves[0..self.moves.len() - 1] {
             comma_separated.push_str(format!("{}", &loc).as_str());
             comma_separated.push_str(", ");
         }
 
-        comma_separated.push_str(format!("{}", &self.0[self.0.len() - 1]).as_str());
+        comma_separated.push_str(format!("{}", &self.moves[self.moves.len() - 1]).as_str());
         write!(f, "{}", comma_separated)
     }
 }
 
 impl Moves {
+    pub fn new(locs: Vec<Location>) -> Moves {
+        Moves { moves: locs }
+    }
     pub fn contains(&self, loc: &Location) -> bool {
-        self.0.contains(loc)
+        self.moves.contains(loc)
+    }
+    pub fn sort(&mut self) {
+        self.moves.sort();
     }
 }
